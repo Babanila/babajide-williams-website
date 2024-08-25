5694d68ccf925d3b3c03ed54412e24fd27854693
babanila <babanila@yahoo.com>
2024-08-25 18:16:18 +0200

    Add Button component

---
+++ src/routes/Button.svelte
@@ -0,0 +1,48 @@
+<script>
+	let buttonProps = {
+		class: [$$restProps.class]
+	};
+</script>
+
+<button on:click on:mouseover on:mouseenter on:mouseleave on:focus {...buttonProps}>
+	<slot />
+</button>
+
+<style>
+	button {
+		color: #ffffff;
+		border-radius: 6px;
+		border: #ffffff;
+		padding: 16px;
+		cursor: pointer;
+	}
+
+	.primary {
+		background-color: #4384cd;
+	}
+	.primary:hover {
+		background-color: #1d3557;
+	}
+
+	.secondary {
+		background-color: #f1faee;
+	}
+	.secondary:hover {
+		background-color: #95db4b;
+	}
+
+	.danger {
+		background-color: #e86175;
+	}
+	.danger:hover {
+		background-color: #e61232;
+	}
+
+	.sm {
+		font-size: 1em;
+	}
+
+	.lg {
+		font-size: 2em;
+	}
+</style>
